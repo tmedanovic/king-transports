22b528bb2f4e83c22044c34c2913769961902db9
Tomislav Medanovic <tomislav.medanovic@gmail.com>
2024-03-06 01:06:33 +0100

    added ScopesAndAdminOrAnyOfRolesAuthorize custom attribute and unit tests

---
+++ Services/KingTransports.Common.Tests/Security/ScopesAndAdminOrAnyOfRolesAuthorizeAttributeTests.cs
@@ -0,0 +1,73 @@
+﻿using KingTransports.Common.Security;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.AspNetCore.Routing;
+using Moq;
+using System.Security.Claims;
+
+namespace KingTransports.Common.Tests.Security;
+
+public class ScopesAndAdminOrAnyOfRolesAuthorizeAttributeTests
+{
+    private const string SCOPE_CLAIM_TYPE = "http://schemas.microsoft.com/identity/claims/scope";
+
+    [Theory]
+    [InlineData(new string[] { "api.read" }, null, null, new string[] { "controlor" }, typeof(ForbidResult))]
+    [InlineData(new string[] { "api.read" }, null, null, new string[] { "admin" }, typeof(ForbidResult))]
+    [InlineData(new string[] { "api.read", "api.write" }, null, new string[] { "api.read" }, new string[] { "admin" }, typeof(ForbidResult))]
+    [InlineData(new string[] { "api.read" }, new string[] { "controlor" }, new string[] { "api.read" }, new string[] { "ticket-seller" }, typeof(ForbidResult))]
+    [InlineData(new string[] { "api.read" }, new string[] { "controlor" }, new string[] { "api.read" }, new string[] { "ticket-seller", "controlor" }, null)]
+    [InlineData(null, null, null, new string[] { "admin" }, null)]
+    public void ScopesAndAdminOrAnyOfRolesAuthorizeAttributeTestCases(string[] allowedScopes, string[] allowedRoles, string[] userScopes, string[] userRoles, Type result)
+    {
+        //var filter = new AuthorizationFilterContext(fakeActionContext, new List<IFilterMetadata>() { });
+        var filter = new ScopesAndAdminOrAnyOfRolesAuthorizeAttribute(allowedScopes, allowedRoles);
+        var user = GetTestUser(userScopes, userRoles);
+        var context = BuildContext(user);
+        filter.OnAuthorization(context);
+
+        if(result !=  null)
+        {
+            Assert.Equal(result, context.Result.GetType());
+        }
+        else
+        {
+            Assert.Null(context.Result);
+        }    
+    }
+
+    private AuthorizationFilterContext BuildContext(ClaimsPrincipal claimsPrincipal)
+    {
+        var httpContextMock = new Mock<HttpContext>();
+        httpContextMock.Setup(a => a.User).Returns(claimsPrincipal);
+
+        ActionContext fakeActionContext = new ActionContext(httpContextMock.Object, new RouteData(),
+                                         new Microsoft.AspNetCore.Mvc.Abstractions.ActionDescriptor());
+
+        return new AuthorizationFilterContext(fakeActionContext, new List<IFilterMetadata>());
+    }
+
+    private ClaimsPrincipal GetTestUser(string[] scopes, string[] roles)
+    {
+        var identity = new ClaimsIdentity(new[] {
+            new Claim(ClaimTypes.Name, "Test User")
+        });
+
+        if(scopes != null)
+        {
+            identity.AddClaim(new Claim(SCOPE_CLAIM_TYPE, string.Join(' ', scopes)));
+        }
+
+        if(roles != null)
+        {
+            foreach(var role in roles)
+            {
+                identity.AddClaim(new Claim(ClaimTypes.Role, role));
+            }
+        }
+
+        var claimsPrincipal = new ClaimsPrincipal(identity);
+        return claimsPrincipal;
+    }
+}
---
+++ Services/KingTransports.Common/Security/ScopesAndAdminOrAnyOfRolesAuthorizeAttribute.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace KingTransports.Common.Security
+{
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method)]
+    public class ScopesAndAdminOrAnyOfRolesAuthorizeAttribute : Attribute, IAuthorizationFilter
+    {
+        private const string SCOPE_CLAIM_TYPE = "http://schemas.microsoft.com/identity/claims/scope";
+        private readonly string[] _scopes;
+        private readonly string[] _roles;
+
+        public ScopesAndAdminOrAnyOfRolesAuthorizeAttribute(string[] scopes, string[] roles)
+        {
+            _scopes = scopes;
+            _roles = roles;
+        }
+
+        public void OnAuthorization(AuthorizationFilterContext context)
+        {
+            bool isAuthorized = HasAllScopes(context.HttpContext.User) && HasAnyRole(context.HttpContext.User);
+
+            if (!isAuthorized)
+            {
+                context.Result = new ForbidResult();
+            }
+        }
+
+        private bool HasAnyRole(ClaimsPrincipal principal)
+        {
+            if (principal.IsInRole("admin"))
+            {
+                return true;
+            }
+
+            if (_roles == null)
+            {
+                return true;
+            }
+
+            return _roles.Any(principal.IsInRole);
+        }
+
+        private bool HasAllScopes(ClaimsPrincipal principal)
+        {
+            if (_scopes == null)
+            {
+                return true;
+            }
+
+            var claim = principal.FindFirst(SCOPE_CLAIM_TYPE);
+
+            if (claim == null)
+            {
+                return false;
+            }
+
+            return !_scopes.Except(claim.Value.Split(' ')).Any();
+        }
+    }
+}
