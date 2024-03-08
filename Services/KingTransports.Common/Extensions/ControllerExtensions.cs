e7e26c9bb0d4f0ad84077668c3b2c1299070f8fc
Tomislav Medanovic <tomislav.medanovic@gmail.com>
2024-03-08 01:27:45 +0100

    added common paged list

---
+++ Services/KingTransports.Common/Extensions/ControllerExtensions.cs
@@ -0,0 +1,28 @@
+﻿using KingTransports.Common.Collections;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Text.Json;
+
+namespace KingTransports.Common.Extensions
+{
+    public static class ControllerExtensions
+    {
+        private static void AddPaginationHeader(this HttpResponse response, int currentPage, int itemsPerPage, int totalItems, int totalPages)
+        {
+            var paginationHeader = new PaginationHeader(currentPage, itemsPerPage, totalItems, totalPages);
+            var options = new JsonSerializerOptions()
+            {
+                PropertyNamingPolicy = JsonNamingPolicy.CamelCase
+            };
+            response.Headers.Add("Pagination", JsonSerializer.Serialize(paginationHeader, options));
+            response.Headers.Add("Access-Control-Expose-Headers", "Pagination");
+        }
+
+        public static OkObjectResult OkPaged<T>(this ControllerBase controller, PagedList<T> list)
+        {
+            controller.Response.AddPaginationHeader(list.CurrentPage, list.PageSize, list.TotalCount, list.TotalPages);
+            return controller.Ok(list);
+            
+        }
+    }
+}
